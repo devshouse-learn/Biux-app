fbeb0e56fe18e374a3f31ad1f83895e79489aaf3
Sleyner Efrain Rodriguez Soto <92marodriguezefrain@gmail.com>
2022-07-29 12:55:08 -0500

    stories repository testing and fixes

--- lib/data/models/reaction_story.dart
+++ lib/data/models/reaction_story.dart
@@ -2,18 +2,18 @@ import 'package:biux/data/models/story.dart';
 import 'package:biux/data/models/user.dart';
 
 class ReactionStory {
-  int? id;
-  int? userId;
+  String id;
+  String userId;
 
   ReactionStory({
-    this.id,
-    this.userId,
+    this.id = '',
+    this.userId = '',
   });
 
-  ReactionStory.fromJson(Map json) {
-    this.id = json["id"];
-    this.userId = json["userId"];
-  }
+ factory ReactionStory.fromJson(Map json) => ReactionStory(
+    id: json["id"] ?? '',
+    userId: json["userId"] ?? '',
+  );
 
   Map<String, dynamic> toJson() => {
         "id": id,
